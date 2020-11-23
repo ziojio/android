ce4a21235ef7cdd5850ea2cd973e5c331a1151ab
ziojio <709145231@qq.com>
2020-11-23 21:26:01 +0800

    zui res

--- app/src/main/java/com/zhuj/android/model/KeyValue.java
+++ app/src/main/java/com/zhuj/android/model/KeyValue.java
@@ -10,20 +10,29 @@ public class KeyValue<Key, Value> {
     public KeyValue(Key key, Value value) {
         this.key = key;
         this.value = value;
-        Map<String, Object> map = new HashMap<>();
     }
 
     public Key getKey() {
         return key;
     }
 
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
     public Value getValue() {
         return value;
     }
 
-    @Override
-    public String toString() {
-        return key + ": " + value;
+    public void setValue(Value value) {
+        this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return "KeyValue{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
 }
